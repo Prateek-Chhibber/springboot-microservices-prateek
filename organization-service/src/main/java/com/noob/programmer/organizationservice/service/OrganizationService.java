be9a517b1ec456e78b8fc35a81b544c3d2304fac
prateekchhibber <0392pc@gmail.com>
2023-06-20 17:03:14 +0530

    Build Save Organization and GET Organization REST API Build

---
+++ organization-service/src/main/java/com/noob/programmer/organizationservice/service/OrganizationService.java
@@ -0,0 +1,10 @@
+package com.noob.programmer.organizationservice.service;
+
+import com.noob.programmer.organizationservice.dto.OrganizationDto;
+
+public interface OrganizationService {
+
+    OrganizationDto saveOrganization(OrganizationDto organizationDto);
+
+    OrganizationDto getOrganizationByCode(String organizationCode);
+}
---
+++ organization-service/src/main/java/com/noob/programmer/organizationservice/service/impl/OrganizationServiceImpl.java
@@ -0,0 +1,33 @@
+package com.noob.programmer.organizationservice.service.impl;
+
+import com.noob.programmer.organizationservice.dto.OrganizationDto;
+import com.noob.programmer.organizationservice.entity.Organization;
+import com.noob.programmer.organizationservice.mapper.OrganizationMapper;
+import com.noob.programmer.organizationservice.repository.OrganizationRepository;
+import com.noob.programmer.organizationservice.service.OrganizationService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class OrganizationServiceImpl implements OrganizationService {
+
+    private OrganizationRepository organizationRepository;
+
+    @Override
+    public OrganizationDto saveOrganization(OrganizationDto organizationDto) {
+
+        // convert OrganizationDto into Organization jpa entity
+        Organization organization = OrganizationMapper.mapToOrganization(organizationDto);
+
+        Organization savedOrganization = organizationRepository.save(organization);
+
+        return OrganizationMapper.mapToOrganizationDto(savedOrganization);
+    }
+
+    @Override
+    public OrganizationDto getOrganizationByCode(String organizationCode) {
+        Organization organization = organizationRepository.findByOrganizationCode(organizationCode);
+        return OrganizationMapper.mapToOrganizationDto(organization);
+    }
+}
