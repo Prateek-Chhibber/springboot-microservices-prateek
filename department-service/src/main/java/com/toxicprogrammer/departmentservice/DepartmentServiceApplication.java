72a57689f2c7cb8b1a8feeac28146fbf286bf5be
prateekchhibber <0392pc@gmail.com>
2023-06-20 18:25:37 +0530

    Define General API Information using Annotations

--- department-service/src/main/java/com/toxicprogrammer/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/toxicprogrammer/departmentservice/DepartmentServiceApplication.java
@@ -1,10 +1,35 @@
 package com.toxicprogrammer.departmentservice;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department Service REST APIs",
+				description = "Department Service REST APIs documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Prateek",
+						email = "prateek3@gmail.com",
+						url = "https://www.javaguides.net"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.javaguides.net"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department Service Doc",
+				url = "https://www.javaguides.net"
+		)
+)
 @SpringBootApplication
 public class DepartmentServiceApplication {
 
