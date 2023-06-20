474e09ee1c6889b76e3415283f8455a3de0f13d1
prateekchhibber <0392pc@gmail.com>
2023-06-20 18:36:15 +0530

    Customizing Swagger API Documentation using Annotations

--- department-service/src/main/java/com/toxicprogrammer/departmentservice/dto/DepartmentDto.java
+++ department-service/src/main/java/com/toxicprogrammer/departmentservice/dto/DepartmentDto.java
@@ -1,5 +1,6 @@
 package com.toxicprogrammer.departmentservice.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,9 +10,21 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(
+        description = "Department DTO model information"
+)
 public class DepartmentDto {
     private Long id;
+    @Schema(
+            description = "Department Name"
+    )
     private String departmentName;
+    @Schema(
+            description = "Department Description"
+    )
     private String departmentDescription;
+    @Schema(
+            description = "Department Code"
+    )
     private String departmentCode;
 }
